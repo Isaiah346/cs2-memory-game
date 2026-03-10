56059326ca0a84a27bf5f1c7077efeb73cdd6b00
Isaiah346 <0715308011@s.tusd1.org>
2026-03-10 21:59:45 +0000

    Card flipo

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("script started");
+
+function flipCard(cardId) {
+    console.log("click");
+    let card = document.getElementById(cardId);
+    card.style.color = "white";
+    card.style.backgroundColor = "gray";
+}
+
